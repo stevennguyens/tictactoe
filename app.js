9230ff79c1a0ac177da6e53d253f531c64af9d48
stevennguyens <stevennguyyenz@gmail.com>
2022-06-20 11:55:07 -0700

    html css

---
+++ app.js
@@ -0,0 +1,19 @@
+// player factory
+const Player = (name) => {
+    const mark = (card) => {
+
+    }
+    return {name}
+};
+
+const GameBoard = (() => {
+    const gameboard = [];
+    return {gameboard}
+})();
+
+const TicTacToe = (() => {
+
+})
+
+let playerOne = Player('bob');
+console.log(playerOne.name);
